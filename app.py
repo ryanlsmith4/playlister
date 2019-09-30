8fea7d55f901d2acaa386b0a806d2b34029e9b81
Ryan <ryan.l.smith4@gmail.com>
2019-09-30 15:22:38 -0700

    [ADD] MongoCLient needs retryWrites=false thisn deployment does not support it

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template, request, redirect, url_for
 from pymongo import MongoClient
 from bson.objectid import ObjectId
 host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/Playlister')
-client = MongoClient(host=host)
+client = MongoClient(host=f'{host}?retryWrites=false')
 db = client.get_default_database()
 playlists = db.playlists
 
