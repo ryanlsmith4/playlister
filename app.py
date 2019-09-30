b0986b75ab9cdd1efc10ce5095acb1e5557405b9
Ryan <ryan.l.smith4@gmail.com>
2019-09-30 15:12:39 -0700

    [ADD] Procfile

--- app.py
+++ app.py
@@ -22,15 +22,15 @@ def playlists_index():
 @app.route('/playlists/new')
 def playlists_new():
   """Create Playlists"""
-  return render_template('playlists_new.html')
+  return render_template('playlists_new.html', playlist = {}, title='New Playlist')
 
 @app.route('/playlists', methods=['POST'])
 def playlists_submit():
+  """Submit playlists to database"""
   playlist = {
     'title': request.form.get('title'),
     'description': request.form.get('description'),
-    'videos': request.form.get('videos').split(),
-    'drop':request.form.get('drop')
+    'videos': request.form.get('videos').split()
   }
   playlist_id = playlists.insert_one(playlist).inserted_id
   print(playlist_id)
@@ -41,6 +41,31 @@ def playlists_show(playlist_id):
     """Show a single playlist."""
     playlist = playlists.find_one({'_id': ObjectId(playlist_id)})
     return render_template('playlists_show.html', playlist=playlist)
+
+@app.route('/playlists/<playlist_id>/edit')
+def playlists_edit(playlist_id):
+    """Show the edit form for a playlist."""
+    playlist = playlists.find_one({'_id': ObjectId(playlist_id)})
+    return render_template('playlists_edit.html', playlist=playlist, title='Edit Playlist')
+
+@app.route('/playlists/<playlist_id>', methods=['POST'])
+def playlists_update(playlist_id):
+  """Submit an edited playlists"""
+  updated_playlist = {
+      'title': request.form.get('title'),
+      'description': request.form.get('description'),
+      'videos': request.form.get('videos').split()
+  }
+  playlists.update_one(
+    {'_id': ObjectId(playlist_id)},
+    {'$set': updated_playlist})
+  return redirect(url_for('playlists_show', playlist_id=playlist_id))
+
+@app.route('/playlists/<playlist_id>/delete', methods=['POST'])
+def playlists_delete(playlist_id):
+    """Delete one playlist."""
+    playlists.delete_one({'_id': ObjectId(playlist_id)})
+    return redirect(url_for('playlists_index'))
 
 if __name__ == '__main__':
   app.run(debug=True)
